3df892276c7b717bf0162632b6cb73a7a7ed1a07
Christopher <holomekc@gmail.com>
2020-01-22 20:49:24 +0100

    * (holomekc) restructure and extend roles and functions

--- src/bshb-definition.ts
+++ src/bshb-definition.ts
@@ -4,34 +4,15 @@
  * @author Christopher Holomek
  * @since 27.09.2019
  */
+import {ROLES} from "./definition/roles";
+import {FUNCTIONS} from "./definition/function";
+
 export class BshbDefinition {
 
     public static determineFunction(value: string): string {
-        if (value === 'TemperatureLevel' ||
-            value === 'TemperatureOffset' ||
-            value === 'Thermostat' ||
-            value === 'ValveTappet' ||
-            value === 'RoomClimateControl' ||
-            value === 'TemperatureLevel' ||
-            value === 'TemperatureLevelConfiguration') {
-            return 'heating';
-        } else if (value === 'ShutterContact') {
-            // hmm
-            // return '';
-        } else if (value === 'IntrusionDetectionControl' ||
-            value === 'SurveillanceAlarm') {
-            return 'security';
-        } else if (value === 'VentilationDelay') {
-            // not sure...
-            return 'heating';
-        } else if (value === 'CommunicationQuality') {
-            return 'communicationQuality';
-        } else if (value === 'BatteryLevel') {
-            return 'battery';
-        } else if (value === 'AirQualityLevel') {
-            return 'airQuality';
-        } else if (value === 'SmokeSensitivity') {
-            return 'smokeSensitivity';
+        const func = FUNCTIONS[value];
+        if (func !== null && typeof func !== 'undefined') {
+            return func;
         }
         return undefined as unknown as string;
     }
@@ -44,6 +25,7 @@ export class BshbDefinition {
     public static determineType(value: any): string {
         return typeof value;
     }
+
 
     /**
      * Get role of bsh device state
@@ -58,179 +40,15 @@ export class BshbDefinition {
             return 'list';
         }
 
-        if (type === 'binarySwitchState') {
-            if (key === 'on') {
-                return 'switch';
-            }
-        } else if (type === 'temperatureLevelState') {
-            if (key === 'temperature') {
-                return 'value.temperature';
-            }
-        } else if (type === 'colorState') {
-            if (key === 'rgb') {
-                return 'level.color.rgb';
-            }
-        } else if (type === 'climateControlState') {
-            if (key === 'setpointTemperature' ||
-                key === 'setpointTemperatureForLevelEco' ||
-                key === 'setpointTemperatureForLevelComfort') {
-                return 'level.temperature';
-            } else if (key === 'low') {
-                return 'indicator.lowbat';
-            } else if (key === 'boostMode') {
-                return 'switch.boost';
-            } else if (key === 'operationMode') {
-                return 'text';
-            } else if (key === 'showSetpointTemperature' || key === 'summerMode') {
-                return 'switch';
-            } else if (key === 'supportsBoostMode' || key === 'ventilationMode') {
-                return 'indicator';
-            }
-        } else if (type === 'colorTemperatureState') {
-            if (key === 'colorTemperature') {
-                return 'level.color.temperature';
-            }
-        } else if (type === 'multiLevelSwitchState') {
-            if (key === 'level') {
-                return 'level.dimmer';
-            }
-        } else if (type === 'temperatureOffsetState') {
-            if (key === 'offset' || key === 'stepSize' || key === 'minOffset' || key === 'maxOffset') {
-                return 'level';
-            }
-        } else if (type === 'childLockState') {
-            if (key === 'childLock') {
-                return 'text';
-            }
-        } else if (type === 'valveTappetState') {
-            if (key === 'position') {
-                return 'level.valve';
-            } else if (key === 'value') {
-                return 'text';
-            }
-        } else if (type === 'shutterContactState') {
-            if (key === 'value') {
-                // TODO: We could add a mapping for some values. In this case true -> OPEN, false -> CLOSED
-                // sensor.window would be great but this would require a boolean and this is a string
-                // OPEN / CLOSED
-                return 'text';
-            }
-        }
-        // Twinguard
-        else if (type === 'smokeSensitivityState') {
-            if (key === 'smokeSensitivity') {
-                return 'text';
-            } else if (key === 'preAlarmEnabled') {
-                return 'indicator';
-            }
-        } else if (type === 'twinguardNightlyPromiseState') {
-            if (key === 'nightlyPromiseEnabled') {
-                return 'indicator';
-            }
-        } else if (type === 'communicationQualityState') {
-            if (key === 'quality') {
-                return 'text';
-            }
-        } else if (type === 'airQualityLevelState') {
-            if (key === 'combinedRating') {
-                return 'text';
-            } else if (key === 'description') {
-                return 'text';
-            } else if (key === 'temperature') {
-                return 'value.temperature';
-            } else if (key === 'temperatureRating') {
-                return 'text';
-            } else if (key === 'humidity') {
-                return 'value.humidity';
-            } else if (key === 'humidityRating') {
-                return 'text';
-            } else if (key === 'purity') {
-                return 'value';
-            } else if (key === 'purityRating') {
-                return 'text';
-            }
-        } else if (type === 'smokeDetectorCheckState') {
-            if (key === 'value') {
-                return 'text';
-            }
-        } else if (type === 'surveillanceAlarmState') {
-            if (key === 'value') {
-                return 'text';
-            } else if (key === 'incidents') {
-                return 'list';
-            }
-        } else if (type === 'intrusionDetectionControlState') {
-            if (key === 'value') {
-                return 'text';
-            } else if (key === 'triggers') {
-                return 'list';
-            } else if (key === 'actuators') {
-                return 'list';
-            } else if (key === 'armActivationDelayTime') {
-                return 'level';
-            } else if (key === 'alarmActivationDelayTime') {
-                return 'level';
-            }
-        } else if (type === 'hueBridgeSearcherState') {
-            if (key === 'searcherState') {
-                return 'text';
-            } else if (key === 'value') {
-                return 'text';
-            }
-        } else if (type === 'armDisarmPushNotificationState') {
-            if (key === 'state') {
-                return 'text';
-            }
-        } else if (type === 'remoteAccessState') {
-            if (key === 'state') {
-                return 'text';
-            }
-        } else if (type === 'remotePushNotificationState') {
-            if (key === 'state') {
-                return 'text';
-            }
-        } else if (type === 'softwareUpdateState') {
-            if (key === 'swUpdateState' || key === 'swUpdateLastResult' ||
-                key === 'swUpdateAvailableVersion' || key === 'swInstalledVersion') {
-                return 'text';
-            }
-        } else if (type === 'ventilationDelayState') {
-            if (key === 'devices') {
-                return 'list';
-            } else if (key === 'delay') {
-                return 'level';
-            }
-        } else if (type === 'powerMeterState') {
-            if (key === 'powerConsumption' || key === 'energyConsumption') {
-                return 'value.power.consumption';
-            } else if (key === 'energyConsumption') {
-                return 'level';
-            }
-        } else if (type === 'powerSwitchState') {
-            if (key === 'switchState') {
-                return 'text';
-            } else if (key === 'automaticPowerOffTime') {
-                return 'value';
-            }
-        } else if ('powerSwitchProgramState') {
-            if (key === 'operationMode') {
-                return 'text';
-            } else if (key === 'schedule') {
-                return 'state';
-            }
-        } else if ('routingState') {
-            if (key === 'value') {
-                return 'text';
-            }
-        }
+        const roleType = ROLES[type];
 
-        // else if (type === '') {
-        //     if (key === '') {
-        //
-        //     } else if (key === '') {
-        //
-        //     }
-        // }
+        if (roleType !== null && typeof roleType !== 'undefined') {
+            const role = roleType[key];
+
+            if (role !== null && typeof role !== 'undefined') {
+                return role;
+            }
+        }
 
         return 'state';
     }
